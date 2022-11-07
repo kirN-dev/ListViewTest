a80cf7c00a6ee01481ef9dda70dd1beced5ed5c9
Silvan <Silvan@LAB44-00>
2022-11-07 13:47:16 +0300

    Добавьте файлы проекта.

---
+++ ListViewTest/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListViewTest
+{
+    internal class MainViewModel 
+    {
+        public ObservableCollection<ItemModel> Items { get; set; }
+
+        public ItemModel SelectedItem { get; set; }
+
+        public MainViewModel()
+        {
+            Items = new()
+            {
+                new ItemModel{ Name = "Kek", Wieght = 1},
+                new ItemModel{ Name = "fgf", Wieght = 11}
+            };
+        }
+    }
+}
