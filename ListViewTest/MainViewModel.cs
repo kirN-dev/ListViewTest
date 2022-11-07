805c49bce2fc0e77ae3e747331221e0570086257
Silvan <Silvan@LAB44-00>
2022-11-07 18:12:40 +0300

    Обновил интерфейс
    Добавил 3 кнопки взаимодействия и команнды

--- ListViewTest/MainViewModel.cs
+++ ListViewTest/MainViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,16 +11,39 @@ namespace ListViewTest
     internal class MainViewModel 
     {
         public ObservableCollection<ItemModel> Items { get; set; }
-
+        public RelayCommand AddItemCommand{ get; set; }
+        public RelayCommand ChangeItemCommand{ get; set; }
+        public RelayCommand DeleteItemCommand{ get; set; }
         public ItemModel SelectedItem { get; set; }
 
         public MainViewModel()
         {
+            AddItemCommand = new RelayCommand(o => AddItem());
+            ChangeItemCommand = new RelayCommand(o => ChangeItem());
+            DeleteItemCommand = new RelayCommand(o => DeleteItem());
+
             Items = new()
             {
                 new ItemModel{ Name = "Kek", Wieght = 1},
                 new ItemModel{ Name = "fgf", Wieght = 11}
             };
+        }
+
+        private void DeleteItem()
+        {
+            Items.Remove(SelectedItem);
+        }
+
+        private void ChangeItem()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddItem()
+        {
+            var newItem = new ItemModel { Name = SelectedItem.Name, Wieght = SelectedItem.Wieght };
+
+            Items.Add(newItem);
         }
     }
 }
